a3b868e69d2d07a01a00f549cd89dbc88cc70f51
perezchris3599 <kweeziperez712@gmail.com>
2022-05-16 17:13:35 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from data import Articles
+from flask_mysqldb import  MySQL
+from wtforms import form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+#configure sql
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#int MYSQL
+mysql = MySQL(app)
+
+Articles = Articles()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords Do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        
+        #create cursor
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%S, %S, %S, %S)", (name, email, username, password))
+        
+
+        #commit to db
+        mysql.connection.commit()
+
+        #close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article one',
+            'body':'fhrhfbeeriuifbuerbu',
+            'author':'Perez',
+            'create_date':'16-05-2022'
+        },
+         {
+            'id': 2,
+            'title':'Article two',
+            'body':'fhrhfbeeriuifbuerbu',
+            'author':'Jane Doe',
+            'create_date':'17-05-2022'
+        },
+         {
+            'id': 3,
+            'title':'Article three',
+            'body':'fhrhfbeeriuifbuerbu',
+            'author':'Kurt Weller',
+            'create_date':'18-05-2022'
+        },
+
+    ]
+    return articles
